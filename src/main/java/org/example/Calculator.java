f06336d5fd1968f4cbf0e1b00f325f60a3de2011
Samanvay Bhatt <40112453+sam19580@users.noreply.github.com>
2025-10-15 19:01:56 +0530

    Update Calculator.java

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -6,23 +6,21 @@ import java.util.List;
 public class Calculator {
 
 
-
-    
-
     public int add(String numbers) {
         if (numbers.equals("")) {
             return 0;
-        }else if(numbers.contains(",")) {
+        } else if (numbers.contains(",")) {
             String[] nums = numbers.split(",");
-            int num1=Integer.valueOf(nums[0]);
-            int num2=Integer.valueOf(nums[1]);
+            int num1 = Integer.valueOf(nums[0]);
+            int num2 = Integer.valueOf(nums[1]);
 
-            return num1+num2;
+            return num1 + num2;
         }
 
         // Single number case
         else {
-            return num;
+            return Integer.valueOf(numbers);
         }
     }
+}
     
